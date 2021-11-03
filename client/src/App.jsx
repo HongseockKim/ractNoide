bf0bcc77603c5c30a9c6fbcd4cc88d8641e97d10
HongseockKim <gusduswk610@gmail.com>
2021-11-04 00:24:46 +0900

    myreact

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,10 @@
 import './App.css';
+import Landing from "./component/landing";
 function App() {
   return (
     <main>
       <div className="wrapper">
+          <Landing/>
       </div>
     </main>
   );
---
+++ client/src/component/landing.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Landing extends Component {
+    render() {
+        return (
+            <div>
+                메인
+            </div>
+        );
+    }
+}
+
+export default Landing;
